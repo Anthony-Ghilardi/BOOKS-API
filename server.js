400f0c7386a43111e8a9d5c3f641a5716fcce858
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-05-22 22:59:59 -0700

    Added CORS and retested postman/browser functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
+const cors = require('cors')
 const app = express();
 
 // Express Settings
@@ -11,6 +12,12 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.json())
+app.use(cors())
+
+// Cors config for all CORS requests
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
 
 // Controllers and Routes
 app.use('/books', require('./controllers/books-controller'))
@@ -25,4 +32,7 @@ app.get('*', (req, res) => {
 })
 
 // Listens for connection to port 3000
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000, function () {
+    console.log('CORS-enabled web server listening on port ' + (process.env.PORT || 3000));
+  });
+  
